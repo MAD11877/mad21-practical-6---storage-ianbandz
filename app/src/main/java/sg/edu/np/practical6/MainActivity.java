030a320e30ba315adae8d04d79309585da3716dc
Jeng Seong Ian Wee <s10203038@connect.np.edu.sg>
2021-06-13 15:15:36 +0800

    fin

---
+++ app/src/main/java/sg/edu/np/practical6/MainActivity.java
@@ -0,0 +1,98 @@
+package sg.edu.np.practical6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<User> userList = new ArrayList<>();
+    User u = new User();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Debug", "create");
+
+        Intent rec = getIntent();
+        int value = rec.getIntExtra("id",0);
+        for(int i = 0; i < 10; i++){
+            u.name = String.valueOf(i);
+            userList.add(u);
+        }
+//        u.name = "MAD";
+//        u.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+//        u.id = 1;
+//        u.followed = false;
+
+        TextView name = findViewById(R.id.txtName);
+        name.setText(u.name);
+        TextView description = findViewById(R.id.txtDescription);
+        description.setText(u.description);
+        setFollowBtn();
+    }
+
+    private void setFollowBtn() {
+        Button b = findViewById(R.id.btnFollow);
+        if(u.followed) {
+            b.setText("Unfollow");
+        }
+        else {
+            b.setText("Follow");
+        }
+    }
+    public void onFollowClick(View v) {
+        u.followed = !u.followed;
+        if(u.followed)
+            Toast.makeText(this, "Followed", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this,"Unfollowed", Toast.LENGTH_SHORT).show();
+        setFollowBtn();
+
+        MyDBHandler db = new MyDBHandler(this);
+        db.updateUser(u);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Debug", "start");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Debug", "stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Debug", "destroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Debug", "pause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Debug", "resume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Debug", "restart");
+    }
+}
---
+++ app/src/main/java/sg/edu/np/practical6/User.java
@@ -0,0 +1,8 @@
+package sg.edu.np.practical6;
+
+public class User {
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
+}
